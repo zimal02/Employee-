01b7676cbc0620dfd38d3b7d6bbbcfdd0b46d0f8
zimalriaz <zimalriaz.ext@seamless.se>
2026-03-11 02:09:45 +0500

    initial commit

---
+++ src/maincar.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+                                //String Manipulation
+public class maincar {
+    public static void main(String[] args) {
+        /*Car<String> car = new Car<String>("Mercedy", "4");
+        System.out.println(car.getTyre());
+        System.out.println(car.getName());
+
+        ArrayList<String> month = new ArrayList<>();
+        month.add("January");
+        month.add("march");
+
+        System.out.println(month);
+        ArrayList<Integer> num = new ArrayList<>();
+        num.add(01);
+        num.add(02);
+        System.out.println(num);*/
+
+
+        Car<String> car = new Car<String>("Mercedy", "4");
+        System.out.println(car.getTyre());
+        System.out.println(car.getName());
+
+        ArrayList<String> month = new ArrayList<>();
+        month.add("January");
+        month.add("march");
+        System.out.println(month);
+        ArrayList<Integer> num = new ArrayList<>();
+        num.add(01);
+        num.add(02);
+        System.out.println(num);
+    }
+}
+
+                                        //Generics
+        /*ArrayList<Integer> num2 = new ArrayList<>();
+        num2.add(3);
+        num2.add(4);
+        num2.add(5);
+        System.out.println("list numbers are:"+ num2);
+       Car.printdata(num2);
+       Car.adddata(num2);*/
+
+
+
+
+
+
+
+
+
+
